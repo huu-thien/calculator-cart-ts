bff1eacef26e2d97b38d96310452787ebd61d941
huu-thien <86077619+huu-thien@users.noreply.github.com>
2023-07-30 02:07:14 +0700

    calculator cart

---
+++ src/components/CartLineItem.tsx
@@ -0,0 +1,103 @@
+import { ReactElement, ChangeEvent, memo } from "react";
+import {
+  CartItemType,
+  ReducerAction,
+  ReducerActionType,
+} from "../context/CartProvider";
+
+type PropsType = {
+  item: CartItemType;
+  dispatch: React.Dispatch<ReducerAction>;
+  REDUCER_ACTIONS: ReducerActionType;
+};
+
+const CartLineItem = ({ item, dispatch, REDUCER_ACTIONS }: PropsType) => {
+  const img: string = new URL(`../images/${item.id}.jpg`, import.meta.url).href;
+
+  const lineTotal: number = item.quantity * item.price;
+
+  const highestQuantity: number = 20 > item.quantity ? 20 : item.quantity;
+
+  const optionValues: number[] = [...Array(highestQuantity).keys()].map(
+    (i) => i + 1
+  );
+  const options: ReactElement[] = optionValues.map((val) => {
+    return (
+      <option key={`opt${val}`} value={val}>
+        {val}
+      </option>
+    );
+  });
+
+  const onChangeQuantity = (e: ChangeEvent<HTMLSelectElement>) => {
+    dispatch({
+      type: REDUCER_ACTIONS.QUANTITY,
+      payload: { ...item, quantity: Number(e.target.value) },
+    });
+  };
+
+  const onremoveFromCart = () =>
+    dispatch({ type: REDUCER_ACTIONS.REMOVE, payload: item });
+
+  const content = (
+    <li className="cart__item">
+      <img src={img} alt={item.name} className="cart__img" />
+      <div aria-label="Item name">{item.name}</div>
+      <div aria-label="Price Per item">
+        {new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(item.price)}
+      </div>
+      <label htmlFor="itemQty" className="offscreen">
+        Item Quantity
+      </label>
+      <select
+        name="itemQty"
+        id="itemQty"
+        className="cart__select"
+        value={item.quantity}
+        aria-lable="Item Quantity"
+        onChange={onChangeQuantity}
+      >
+        {options}
+      </select>
+
+      <div className="cart__item-subtotal" aria-label="Line Item Subtotal">
+        {new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(lineTotal)}
+      </div>
+
+      <button
+        className="cart__button"
+        aria-label="Remove Item From Cart"
+        title="Remove Item From Cart"
+        onClick={onremoveFromCart}
+      >
+        ❌
+      </button>
+    </li>
+  );
+  return content;
+};
+
+function areItemsEqual(
+  { item: prevItem }: PropsType,
+  { item: nextItem }: PropsType
+) {
+  return Object.keys(prevItem).every((key) => {
+    return (
+      prevItem[key as keyof CartItemType] ===
+      nextItem[key as keyof CartItemType]
+    );
+  });
+}
+
+const MemoizedCartLineItem = memo<typeof CartLineItem>(
+  CartLineItem,
+  areItemsEqual
+);
+
+export default MemoizedCartLineItem;
